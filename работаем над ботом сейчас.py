54ae48ae4748947ca627db09cd1f60247f00384f
Богдан <bogdan555551@mail.ru>
2022-03-22 16:43:01 +0300

    карты

---
+++ работаем над ботом сейчас.py
@@ -0,0 +1,100 @@
+import bs4
+import requests
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('5176514754:AAHx4jWsOtLAacgQ6C5-T9D6FHe1y6wnwEo')
+
+
+@bot.message_handler(commands=["start"])
+def start(message, res=False):
+    chat_id = message.chat.id
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("!Главное меню")
+    btn2 = types.KeyboardButton("Помощь")
+    markup.add(btn1, btn2)
+
+    bot.send_message(chat_id,
+                     text="Привет, {0.first_name}! Я тестовый бот для курса программирования на языке ПаЙтон".format(
+                         message.from_user), reply_markup=markup)
+
+
+# Получение сообщений от юзера
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    global game21
+
+    chat_id = message.chat.id
+    ms_text = message.text
+
+    if ms_text == "Главное меню" or ms_text == "!Главное меню" or ms_text == "Вернуться в главное меню":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Развлечения")
+        btn2 = types.KeyboardButton("Карту")
+        btn3 = types.KeyboardButton("Управление")
+        back = types.KeyboardButton("Помощь")
+        markup.add(btn1, btn2, btn3, back)
+        bot.send_message(chat_id, text="Вы в главном меню", reply_markup=markup)
+
+    elif ms_text == "Развлечения":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Прислать собаку")
+        btn2 = types.KeyboardButton("Прислать анекдот")
+        back = types.KeyboardButton("Вернуться в главное меню")
+        markup.add(btn1, btn2, back)
+        bot.send_message(chat_id, text="Развлечения", reply_markup=markup)
+
+    elif ms_text == "/dog" or ms_text == "Прислать собаку":
+
+        contents = requests.get('https://random.dog/woof.json').json()
+        urLDOG = contents['url']
+        bot.send_photo(chat_id, photo=urLDOG, caption="Вот и пёс")
+
+    elif ms_text == "Прислать анекдот":
+        bot.send_message(chat_id, text=get_anekdot())
+
+
+    elif ms_text == "Карту":
+        if game21 == None:
+            goto_menu(chat_id, "Выход")
+            return
+        text_game = game21.get_cards(1)
+        bot.send_media_group(chat_id, media=getMediaCards(game21))
+        bot.send_message(chat_id, "Выход")
+
+        if game21.status != None:
+            goto_menu(chat_id, "Выход")
+            return
+
+    elif ms_text == "Управелние":
+        bot.send_message(chat_id, text="ещё не готово")
+
+    elif ms_text == "Помощь" or ms_text == "/help":
+
+        bot.send_message(chat_id, "Автор: Манских Богдан")
+        key1 = types.InlineKeyboardMarkup()
+        btn1 = types.InlineKeyboardButton(text="Напишите автору", url="id1")
+        key1.add(btn1)
+        img = open('Манских Богдан.png', 'rb')
+        bot.send_photo(message.chat.id, img, reply_markup=key1)
+
+    else:
+
+
+        bot.send_message(chat_id, text="я слышу, сообщение:"+ ms_text)
+
+# -----------------------------------------------------------------------
+def get_anekdot():
+    array_anekdots = []
+    req_anek = requests.get('http://anekdotme.ru/random')
+    soup = bs4.BeautifulSoup(req_anek.text, "html.parser")
+    result_find = soup.select('.anekdot_text')
+    for result in result_find:
+        array_anekdots.append(result.getText().strip())
+    return array_anekdots[0]
+
+# -----------------------------------------------------------------------
+bot.polling(none_stop=True, interval=0)  # Запускаем бота
+
+print()
--- работаем над ботом.py
+++ работаем над ботом.py
@@ -1,3 +1,5 @@
+
+
 import bs4
 import requests
 import telebot
@@ -53,7 +55,7 @@ def get_text_messages(message):
         bot.send_message(chat_id, text=get_anekdot())
 
 
-    elif ms_text == "WEB-камера":
+    elif ms_text == "Карту":
         bot.send_message(chat_id, text="ещё не готово")
 
     elif ms_text == "Управелние":
@@ -82,6 +84,73 @@ def get_anekdot():
     for result in result_find:
         array_anekdots.append(result.getText().strip())
     return array_anekdots[0]
+
+class Game21:
+    def __init__(self, deck_count=1):
+        new_pack = self.new_pack(deck_count)  # в конструкторе создаём новую пачку из deck_count-колод
+        if new_pack is not None:
+            self.pack_card = new_pack  # сформированная колода
+            self.remaining = new_pack["remaining"],  # количество оставшихся карт в колоде
+            self.card_in_game = []  # карты в игре
+            self.arr_cards_URL = []  # URL карт игрока
+            self.score = 0  # очки игрока
+            self.status = None  # статус игры, True - игрок выиграл, False - Игрок проиграл, None - Игра продолжается
+
+    # ---------------------------------------------------------------------
+    def new_pack(self, deck_count):
+        response = requests.get(f"https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count={deck_count}")
+        # создание стопки карт из "deck_count" колод по 52 карты
+        if response.status_code != 200:
+            return None
+        pack_card = response.json()
+        return pack_card
+
+    # ---------------------------------------------------------------------
+    def get_cards(self, card_count=1):
+        if self.pack_card == None:
+            return None
+        if self.status != None:  # игра закончена
+            return None
+
+        deck_id = self.pack_card["deck_id"]
+        response = requests.get(f"https://deckofcardsapi.com/api/deck/{deck_id}/draw/?count={card_count}")
+        # достать из deck_id-колоды card_count-карт
+        if response.status_code != 200:
+            return False
+
+        new_cards = response.json()
+        if new_cards["success"] != True:
+            return False
+        self.remaining = new_cards["remaining"]  # обновим в классе количество оставшихся карт в колоде
+
+        arr_newCards = []
+        for card in new_cards["cards"]:
+            card_obj = Card(card)  # создаем объекты класса Card и добавляем их в список карт у игрока
+            arr_newCards.append(card_obj)
+            self.card_in_game.append(card_obj)
+            self.score = self.score + card_obj.cost
+            self.arr_cards_URL.append(card["image"])
+
+        if self.score > 21:
+            self.status = False
+            text_game = "Очков: " + str(self.score) + " ВЫ ПРОИГРАЛИ!"
+
+        elif self.score == 21:
+            self.status = True
+            text_game = "ВЫ ВЫИГРАЛИ!"
+        else:
+            self.status = None
+            text_game = "Очков: " + str(self.score) + " в колоде осталось карт: " + str(self.remaining)
+
+        return text_game
+
+
+# -----------------------------------------------------------------------
+if __name__ == "__main__":
+    print("Этот код должен использоваться ТОЛЬКО в качестве модуля!")
+
+
+
 
 # -----------------------------------------------------------------------
 bot.polling(none_stop=True, interval=0)  # Запускаем бота
