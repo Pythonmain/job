1cf570109f3412130f6c842b7fec59e72143e16c
Богдан <bogdan555551@mail.ru>
2022-03-29 16:28:21 +0300

    29.03.22

--- работаем над ботом.py
+++ работаем над ботом.py
@@ -1,158 +1,228 @@
+# Телеграм-бот v.004
+import json
+from gettext import find
+from io import BytesIO
 
-
-import bs4
-import requests
 import telebot
 from telebot import types
+import requests
+import bs4
+import botGames
+from menuBot import Menu
+# import DZ  # домашнее задание от первого урока
 
 bot = telebot.TeleBot('5176514754:AAHx4jWsOtLAacgQ6C5-T9D6FHe1y6wnwEo')
+game21 = None
 
 
-@bot.message_handler(commands=["start"])
-def start(message, res=False):
-    chat_id = message.chat.id
+# -----------------------------------------------------------------------
 
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("!Главное меню")
-    btn2 = types.KeyboardButton("Помощь")
-    markup.add(btn1, btn2)
-
-    bot.send_message(chat_id,
-                     text="Привет, {0.first_name}! Я тестовый бот для курса программирования на языке ПаЙтон".format(
-                         message.from_user), reply_markup=markup)
+@bot.message_handler(commands="start")
+def command(message, res=False):
+    txt_message = f"Привет, {message.from_user.first_name}! Я тестовый бот для курса программирования на языке Python"
+    bot.send_message(message.chat.id, text=txt_message, reply_markup=Menu.getMenu("Главное меню").markup)
 
 
+# -----------------------------------------------------------------------
 # Получение сообщений от юзера
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
+    global game21
+
     chat_id = message.chat.id
     ms_text = message.text
 
-    if ms_text == "Главное меню" or ms_text == "!Главное меню" or ms_text == "Вернуться в главное меню":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("Развлечения")
-        btn2 = types.KeyboardButton("WEB-камера")
-        btn3 = types.KeyboardButton("Управление")
-        back = types.KeyboardButton("Помощь")
-        markup.add(btn1, btn2, btn3, back)
-        bot.send_message(chat_id, text="Вы в главном меню", reply_markup=markup)
+    result = goto_menu(chat_id, ms_text)
+    if result == True:
+        return
 
-    elif ms_text == "Развлечения":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("Прислать собаку")
-        btn2 = types.KeyboardButton("Прислать анекдот")
-        back = types.KeyboardButton("Вернуться в главное меню")
-        markup.add(btn1, btn2, back)
-        bot.send_message(chat_id, text="Развлечения", reply_markup=markup)
+    if Menu.cur_menu != None and ms_text in Menu.cur_menu.buttons:
 
-    elif ms_text == "/dog" or ms_text == "Прислать собаку":
+        if ms_text == "Помощь":
+            send_help(chat_id)
 
-        contents = requests.get('https://random.dog/woof.json').json()
-        urLDOG = contents['url']
-        bot.send_photo(chat_id, photo=urLDOG, caption="Вот и пёс")
+        elif ms_text == "Прислать собаку":
+            bot.send_photo(chat_id, photo=get_dogURL(), caption="Вот тебе собачка!")
 
-    elif ms_text == "Прислать анекдот":
-        bot.send_message(chat_id, text=get_anekdot())
+        elif ms_text == "Прислать анекдот":
+            bot.send_message(chat_id, text=get_anekdot())
 
+        elif ms_text == "Прислать фильм":
+            send_film(chat_id)
 
-    elif ms_text == "Карту":
-        bot.send_message(chat_id, text="ещё не готово")
+        elif ms_text == "Угадай кто?":
+            get_ManOrNot(chat_id)
 
-    elif ms_text == "Управелние":
-        bot.send_message(chat_id, text="ещё не готово")
+        elif ms_text == "Карту!":
+            if game21 == None:
+                goto_menu(chat_id, "Выход")
+                return
 
-    elif ms_text == "Помощь" or ms_text == "/help":
+            text_game = game21.get_cards(1)
+            bot.send_media_group(chat_id, media=getMediaCards(game21))
+            bot.send_message(chat_id, text=text_game)
 
-        bot.send_message(chat_id, "Автор: Манских Богдан")
-        key1 = types.InlineKeyboardMarkup()
-        btn1 = types.InlineKeyboardButton(text="Напишите автору", url="id1")
-        key1.add(btn1)
-        img = open('Манских Богдан.png', 'rb')
-        bot.send_photo(message.chat.id, img, reply_markup=key1)
+            if game21.status != None:
+                goto_menu(chat_id, "Выход")
+                return
 
+        elif ms_text == "Стоп!":
+            game21 = None
+            goto_menu(chat_id, "Выход")
+            return
+
+        elif ms_text == "Задание-1":
+            DZ.dz1(bot, chat_id)
+
+        elif ms_text == "Задание-2":
+            DZ.dz2(bot, chat_id)
+
+        elif ms_text == "Задание-3":
+            DZ.dz3(bot, chat_id)
+
+        elif ms_text == "Задание-4":
+            DZ.dz4(bot, chat_id)
+
+        elif ms_text == "Задание-5":
+            DZ.dz5(bot, chat_id)
+
+        elif ms_text == "Задание-6":
+            DZ.dz6(bot, chat_id)
+
+    else:  # ...........................................................................................................
+        bot.send_message(chat_id, text="Мне жаль, я не понимаю вашу команду: " + ms_text)
+        goto_menu(chat_id, "Главное меню")
+
+# -----------------------------------------------------------------------
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+
+    pass
+
+# -----------------------------------------------------------------------
+def goto_menu(chat_id, name_menu):
+    # получение нужного элемента меню
+    if name_menu == "Выход" and Menu.cur_menu != None and Menu.cur_menu.parent != None:
+        target_menu = Menu.getMenu(Menu.cur_menu.parent.name)
     else:
+        target_menu = Menu.getMenu(name_menu)
+
+    if target_menu != None:
+        bot.send_message(chat_id, text=target_menu.name, reply_markup=target_menu.markup)
 
 
-        bot.send_message(chat_id, text="я слышу, сообщение:"+ ms_text)
+        if target_menu.name == "Игра в 21":
+            global game21
+            game21 = botGames.Game21()
+            text_game = game21.get_cards(2)
+            bot.send_media_group(chat_id, media=getMediaCards(game21))
+            bot.send_message(chat_id, text=text_game)
+
+        return True
+    else:
+        return False
+
+
+# -----------------------------------------------------------------------
+def getMediaCards(game21):
+    medias = []
+    for url in game21.arr_cards_URL:
+        medias.append(types.InputMediaPhoto(url))
+    return medias
+
+
+# -----------------------------------------------------------------------
+def send_help(chat_id):
+    global bot
+    bot.send_message(chat_id, "Автор: Богдан")
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton(text="Напишите автору", url="0")
+    markup.add(btn1)
+
+
+# -----------------------------------------------------------------------
+def send_film(chat_id):
+    film = get_randomFilm()
+    info_str = f"<b>{film['Наименование']}</b>\n" \
+               f"Год: {film['Год']}\n" \
+               f"Страна: {film['Страна']}\n" \
+               f"Жанр: {film['Жанр']}\n" \
+               f"Продолжительность: {film['Продолжительность']}"
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton(text="Трейлер", url=film["Трейлер_url"])
+    btn2 = types.InlineKeyboardButton(text="СМОТРЕТЬ онлайн", url=film["фильм_url"])
+    markup.add(btn1, btn2)
+    bot.send_photo(chat_id, photo=film['Обложка_url'], caption=info_str, parse_mode='HTML', reply_markup=markup)
+
 
 # -----------------------------------------------------------------------
 def get_anekdot():
     array_anekdots = []
     req_anek = requests.get('http://anekdotme.ru/random')
-    soup = bs4.BeautifulSoup(req_anek.text, "html.parser")
-    result_find = soup.select('.anekdot_text')
-    for result in result_find:
-        array_anekdots.append(result.getText().strip())
-    return array_anekdots[0]
-
-class Game21:
-    def __init__(self, deck_count=1):
-        new_pack = self.new_pack(deck_count)  # в конструкторе создаём новую пачку из deck_count-колод
-        if new_pack is not None:
-            self.pack_card = new_pack  # сформированная колода
-            self.remaining = new_pack["remaining"],  # количество оставшихся карт в колоде
-            self.card_in_game = []  # карты в игре
-            self.arr_cards_URL = []  # URL карт игрока
-            self.score = 0  # очки игрока
-            self.status = None  # статус игры, True - игрок выиграл, False - Игрок проиграл, None - Игра продолжается
-
-    # ---------------------------------------------------------------------
-    def new_pack(self, deck_count):
-        response = requests.get(f"https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count={deck_count}")
-        # создание стопки карт из "deck_count" колод по 52 карты
-        if response.status_code != 200:
-            return None
-        pack_card = response.json()
-        return pack_card
-
-    # ---------------------------------------------------------------------
-    def get_cards(self, card_count=1):
-        if self.pack_card == None:
-            return None
-        if self.status != None:  # игра закончена
-            return None
-
-        deck_id = self.pack_card["deck_id"]
-        response = requests.get(f"https://deckofcardsapi.com/api/deck/{deck_id}/draw/?count={card_count}")
-        # достать из deck_id-колоды card_count-карт
-        if response.status_code != 200:
-            return False
-
-        new_cards = response.json()
-        if new_cards["success"] != True:
-            return False
-        self.remaining = new_cards["remaining"]  # обновим в классе количество оставшихся карт в колоде
-
-        arr_newCards = []
-        for card in new_cards["cards"]:
-            card_obj = Card(card)  # создаем объекты класса Card и добавляем их в список карт у игрока
-            arr_newCards.append(card_obj)
-            self.card_in_game.append(card_obj)
-            self.score = self.score + card_obj.cost
-            self.arr_cards_URL.append(card["image"])
-
-        if self.score > 21:
-            self.status = False
-            text_game = "Очков: " + str(self.score) + " ВЫ ПРОИГРАЛИ!"
-
-        elif self.score == 21:
-            self.status = True
-            text_game = "ВЫ ВЫИГРАЛИ!"
-        else:
-            self.status = None
-            text_game = "Очков: " + str(self.score) + " в колоде осталось карт: " + str(self.remaining)
-
-        return text_game
-
+    if req_anek.status_code == 200:
+        soup = bs4.BeautifulSoup(req_anek.text, "html.parser")
+        result_find = soup.select('.anekdot_text')
+        for result in result_find:
+            array_anekdots.append(result.getText().strip())
+    if len(array_anekdots) > 0:
+        return array_anekdots[0]
+    else:
+        return ""
 
 # -----------------------------------------------------------------------
-if __name__ == "__main__":
-    print("Этот код должен использоваться ТОЛЬКО в качестве модуля!")
-
-
-
-
+def get_dogURL():
+    url = ""
+    req = requests.get('https://random.dog/woof.json')
+    if req.status_code == 200:
+        r_json = req.json()
+        url = r_json['url']
+        # url.split("/")[-1]
+    return url
 # -----------------------------------------------------------------------
+def get_ManOrNot(chat_id):
+    global bot
+
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton(text="Проверить", url="https://vc.ru/dev/58543-thispersondoesnotexist-sayt-generator-realistichnyh-lic")
+    markup.add(btn1)
+
+    req = requests.get("https://thispersondoesnotexist.com/image", allow_redirects=True)
+    if req.status_code == 200:
+        img = BytesIO(req.content)
+        bot.send_photo(chat_id, photo=img, reply_markup=markup, caption="Этот человек реален?")
+
+# ---------------------------------------------------------------------
+def get_randomFilm():
+    url = 'https://randomfilm.ru/'
+    infoFilm = {}
+    req_film = requests.get(url)
+    soup = bs4.BeautifulSoup(req_film.text, "html.parser")
+    result_find = soup.find('div', align="center", style="width: 100%")
+    infoFilm["Наименование"] = result_find.find("h2").getText()
+    names = infoFilm["Наименование"].split(" / ")
+    infoFilm["Наименование_rus"] = names[0].strip()
+    if len(names) > 1:
+        infoFilm["Наименование_eng"] = names[1].strip()
+
+    images = []
+    for img in result_find.findAll('img'):
+        images.append(url + img.get('src'))
+    infoFilm["Обложка_url"] = images[0]
+
+    details = result_find.findAll('td')
+    infoFilm["Год"] = details[0].contents[1].strip()
+    infoFilm["Страна"] = details[1].contents[1].strip()
+    infoFilm["Жанр"] = details[2].contents[1].strip()
+    infoFilm["Продолжительность"] = details[3].contents[1].strip()
+    infoFilm["Режиссёр"] = details[4].contents[1].strip()
+    infoFilm["Актёры"] = details[5].contents[1].strip()
+    infoFilm["Трейлер_url"] = url + details[6].contents[0]["href"]
+    infoFilm["фильм_url"] = url + details[7].contents[0]["href"]
+
+    return infoFilm
+# ---------------------------------------------------------------------
+
+
 bot.polling(none_stop=True, interval=0)  # Запускаем бота
 
 print()
